3810d6600707d0afb98b0045d97b525673555801
more <morefocus@163.com>
2024-03-20 23:55:24 +0800

    行业字典

---
+++ morefocus-admin/src/main/java/cn/morefocus/admin/constant/AdminDictConst.java
@@ -0,0 +1,58 @@
+package cn.morefocus.admin.constant;
+
+import cn.morefocus.base.constant.RedisKeyConst;
+
+/**
+ * 字典 常量类
+ *
+ * @author loki
+ */
+public class AdminDictConst extends RedisKeyConst {
+    /**
+     * 软件使用行业
+     */
+    public static class Industry {
+        /**
+         * 食堂
+         */
+        public static final int CANTEEN = 1;
+
+        /**
+         * 汽修汽配
+         */
+        public static final int AUTO_REPAIR = 2;
+
+        /**
+         * 烟酒食品
+         */
+
+        /**
+         * 家电数码
+         */
+
+        /**
+         * 服装箱包
+         */
+
+        /**
+         * 五金建材
+         */
+
+        /**
+         * 母婴玩具
+         */
+
+        /**
+         * 日化用品
+         */
+
+        /**
+         * 文体图书
+         */
+
+        /**
+         * 其他行业
+         */
+        public static final int OTHER = 100;
+    }
+}
